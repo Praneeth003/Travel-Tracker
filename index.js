bb84c51c99ada5e0d1decb9bf6c865cf0eab6571
Praneeth003 <ch.krishnapraneeth@gmail.com>
2023-10-31 19:41:09 -0400

    Upon get request, the map gets colored with the visited countries already in the db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -7,8 +8,24 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const db = new pg.Client({
+  user: "postgres",
+  password: "",
+  host: "localhost",
+  database: "World",
+  port: 5433
+});
+db.connect();
+
 app.get("/", async (req, res) => {
   //Write your code here.
+  const result = await db.query("Select country_code from visited_countries");
+  console.log(result.rows);
+  const countries = [];
+  result.rows.forEach((i) => {
+    countries.push(i.country_code);
+  });
+  res.render("index.ejs", {countries : countries, total: countries.length});
 });
 
 app.listen(port, () => {
